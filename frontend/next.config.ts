1339b46b14710971bc23828cdf9b4b13c73b0e1a
h083onis <masaki00241117@gmail.com>
2024-12-08 19:20:32 +0900

    [modify] Djangoを導入、設定を追加

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
-const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+// const NEXT_PUBLIC_BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+// destination: `${NEXT_PUBLIC_BACKEND_URL}/api/:path*/`,
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -8,7 +9,8 @@ const nextConfig: NextConfig = {
     return [
       {
         source: `/api/:path*`,
-        destination: `${NEXT_PUBLIC_BACKEND_URL}/api/:path*/`,
+        destination: `http://host.docker.internal:5000/api/:path*/`,
+
       },
     ];
   },
